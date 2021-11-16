e7f44e076a0e25b5ab821ab3182b191f9604f02b
PaulSaltos <paul.saltos000@gmail.com>
2021-11-15 23:08:47 -0500

    HW06 ControlStatements

--- helloWorld.java
+++ helloWorld.java
@@ -1,12 +1,13 @@
-public class helloWorld {
+public class HelloWorld {
 
     public static void main(String[] args) {
  
-        System.out.print("\nHello Objects and Web Worlds from Paul Saltos\n ");    
-		
-        System.out.println("\n multiplicaction tables -> 5\n");
+        System.out.println("Hello Objects and Web Worlds from Arroba Cristian");
+
+        System.out.println("\nTabla del 5\n");
+        
         for(int i = 1; i<=12; i++){
-             System.out.println("5x" + i + "=" + (i*5));                                                     
+             System.out.println("5 x " + i + " = " + 5*i);                                                   
         }
     }
 }
