6a85f4bcdb3402f32511d69fe4d966102c07b26d
AJRUAN0 <alexiz.bolt25@gmail.com>
2021-11-18 11:54:10 -0500

    WS07HellowWorld filesv1.1

--- workshops/ruanoa/unit1/New Folder (2)/FarmSimulator/src/ec/edu/espe/farm/model/Chicken.java
+++ workshops/ruanoa/unit1/New Folder (2)/FarmSimulator/src/ec/edu/espe/farm/model/Chicken.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ec.edu.espe.farm.model;
 
 /**
@@ -9,5 +6,36 @@ package ec.edu.espe.farm.model;
  * @author ASUS
  */
 public class Chicken {
+    private String name;
+    private String color;
+    private int age;
+    private boolean isMolting;
     
+    public void doStuff(){
+        
+    }
+    
+    private void cluck(){
+        
+    }
+    
+    private void wander(){
+        
+    }
+    
+    private void eat(){
+        
+    }
+    
+    private void drink(){
+        
+    }
+    
+    private void poop(){
+        
+    }
+    
+    private void layAnEgg(){
+        
+    }
 }
