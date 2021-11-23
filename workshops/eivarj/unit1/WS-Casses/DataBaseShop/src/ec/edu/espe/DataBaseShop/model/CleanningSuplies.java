606f398e8b4fde6a6eaa5894930fef233a15268d
JaimeMauricioEivar <jaimemeivard@hotmail.com>
2021-11-22 23:22:24 -0500

    CLASESGRUPO2

---
+++ workshops/eivarj/unit1/WS-Casses/DataBaseShop/src/ec/edu/espe/DataBaseShop/model/CleanningSuplies.java
@@ -0,0 +1,18 @@
+
+package ec.edu.espe.DataBaseShop.model;
+
+/**
+ *
+ * Grupo2
+ */
+public class CleanningSuplies {
+    private int Product_Id;
+    private String NameOfTheProduct;
+    private int Availability;
+    private Float Price;
+    
+    public void AboutProduct(){
+            }
+    public void sell (){
+    }      
+}
