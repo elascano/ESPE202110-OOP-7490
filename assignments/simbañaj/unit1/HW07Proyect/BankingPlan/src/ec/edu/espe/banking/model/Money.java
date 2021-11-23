ac53f332b0cd48ad6673f18d055eb5f19ea3cd00
jgsimbana <gusjs84@gmail.com>
2021-11-22 23:53:11 -0500

    HW07Proyect

---
+++ assignments/simbañaj/unit1/HW07Proyect/BankingPlan/src/ec/edu/espe/banking/model/Money.java
@@ -0,0 +1,21 @@
+
+package ec.edu.espe.banking.model;
+
+/**
+ *
+ * @author RocketTeam
+ */
+public class Money {
+
+    private float minValue;
+    private float maxValue;
+
+    private void estimateMaximun() {
+
+    }
+
+    private void estimateMinimun() {
+
+    }
+}
+
