b733d2cdae207707b51764c86d1a4cab80c2351f
Your milandazuri <teo.landa08@gmail.com>
2021-12-08 22:05:27 -0500

    unit1

--- workshops/landazurim/unit1/WS11Associations/Farm/src/ec/edu/espe/farm/model/Chicken.java
+++ workshops/landazurim/unit1/WS11Associations/Farm/src/ec/edu/espe/farm/model/Chicken.java
@@ -13,28 +13,28 @@ public class Chicken {
     private String name;
     private String color;
     private int age;
-    private boolean isMolting;
+    private boolean molting;
+
+    @Override
+    public String toString() {
+        return "Chicken{" + "id=" + id + ", name=" + name + ", color=" + color + ", age=" + age + ", molting=" + molting + '}';
+    }
     
-    public void doStuff(int forTime){
-    cluck();
-    wander();
-    eat();
+    
+    public void doStuff(int forTime)
+    {
     }
     
     private void cluck(){
-        System.out.println("This chicken in clucking");
     }
     
     private void wander(){
-        System.out.println("This chicken is wandering");
     }
     
     private void eat(){
-        System.out.println("This chicken is eating");
     }
     
     private void drink(){
-        System.out.println("this chicken is drinking");;
     }
     
     private Poop poop(){
@@ -42,27 +42,25 @@ public class Chicken {
    }
     
     private Egg layAnEgg(){
-         return new Egg();
+         return new Egg(1);
     }
     
-            
     public Chicken( ) {
         id=0;
         name="ch";
         color="white";
         age=0;
-        isMolting=false;
+        molting=true;
     }
-    public Chicken(int id, String name, String color, int age, boolean isMolting) {
+        
+    public Chicken(int id, String name, String color, int age, boolean molting) {
         this.id = id;
         this.name = name;
         this.color = color;
         this.age = age;
-        this.isMolting = isMolting;
+        this.molting = molting;
     }
 
-    
-    
     /**
      * @return the id
      */
@@ -120,17 +118,18 @@ public class Chicken {
     }
 
     /**
-     * @return the isMolting
+     * @return the molting
      */
-    public boolean isIsMolting() {
-        return isMolting;
-    }
-        /**
-     * @param isMolting the isMolting to set
-     */
-    public void setIsMolting(boolean isMolting) {
-        this.isMolting = isMolting;
+    public boolean isMolting() {
+        return molting;
     }
 
-}
+    /**
+     * @param molting the molting to set
+     */
+    public void setMolting(boolean molting) {
+        this.molting = molting;
+    }
+    }        
 
+    
