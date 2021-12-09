143b99b0010d1418549a73ed200a82d5b52deaef
Andersmn19 <andersonmoises19@gmail.com>
2021-12-08 21:32:56 -0500

    unit1

--- workshops/almachea/Unit1/WS011Associations/Farm.java/src/ec/edu/espe/farm/model/Chicken.java
+++ workshops/almachea/Unit1/WS011Associations/Farm.java/src/ec/edu/espe/farm/model/Chicken.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ec.edu.espe.farm.model;
 
 /**
@@ -13,7 +10,16 @@ public class Chicken {
     private String name;
     private String color;
     private int age;
-    private boolean isMolting;
+    private boolean isMolting; 
+
+    
+
+    @Override
+    public String toString() {
+        return "Chicken{" + "id=" + id + ", name=" + name + ", color=" + color + ", age=" + age + ", isMolting=" + isMolting + '}';
+    }
+    
+    
     
     public void doStuff(int forTime){
         cluck();
@@ -40,10 +46,10 @@ public class Chicken {
     
     private Egg layAnEgg(){
         
-        return new Egg();
+        return new Egg(1);
         
-    }
-
+    }   
+    
     public Chicken() {
         id=0;
         name="ch";
@@ -52,7 +58,7 @@ public class Chicken {
         isMolting=false;
     }
 
-    public Chicken(int id, String name, String color, int age, boolean isMolting) {
+     public Chicken(int id, String name, String color, int age, boolean isMolting) {
         this.id = id;
         this.name = name;
         this.color = color;
@@ -60,74 +66,45 @@ public class Chicken {
         this.isMolting = isMolting;
     }
     
-
-    /**
-     * @return the id
-     */
+ 
     public int getId() {
         return id;
     }
 
-    /**
-     * @param id the id to set
-     */
     public void setId(int id) {
         this.id = id;
     }
 
-    /**
-     * @return the name
-     */
     public String getName() {
         return name;
     }
 
-    /**
-     * @param name the name to set
-     */
     public void setName(String name) {
         this.name = name;
     }
 
-    /**
-     * @return the color
-     */
     public String getColor() {
         return color;
     }
 
-    /**
-     * @param color the color to set
-     */
     public void setColor(String color) {
         this.color = color;
     }
 
-    /**
-     * @return the age
-     */
     public int getAge() {
         return age;
     }
 
-    /**
-     * @param age the age to set
-     */
     public void setAge(int age) {
         this.age = age;
     }
 
-    /**
-     * @return the asMolting
-     */
-    public boolean isAsMolting() {
+    public boolean isIsMolting() {
         return isMolting;
     }
 
-    /**
-     * @param isMolting the asMolting to set
-     */
-    public void setAsMolting(boolean isMolting) {
+    public void setIsMolting(boolean isMolting) {
         this.isMolting = isMolting;
     }
+    
 }
