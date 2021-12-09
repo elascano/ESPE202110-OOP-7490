fa5eba034f8456d6bd31a06d3b12cfbfda051783
dyasumaza <dilanalexander78@gmail.com>
2021-12-08 21:25:20 -0500

    version04

--- assignments/asumazad/unit1/HW04FarmIntroduceData/Farm/src/ec/edu/espe/farm/model/Chicken.java
+++ assignments/asumazad/unit1/HW04FarmIntroduceData/Farm/src/ec/edu/espe/farm/model/Chicken.java
@@ -1,3 +1,7 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package ec.edu.espe.farm.model;
 
 /**
@@ -5,13 +9,20 @@ package ec.edu.espe.farm.model;
  * @author dylanasumaza
  */
 public class Chicken {
-    private int id;
+    private int id; 
     private String name;
     private String color;
     private int age;
-    private boolean isMolting;
+    private boolean isMolting; 
+
     
-   
+
+    @Override
+    public String toString() {
+        return "Chicken{" + "id=" + id + ", name=" + name + ", color=" + color + ", age=" + age + ", isMolting=" + isMolting + '}';
+    }
+    
+    
     
     public void doStuff(int forTime){
         cluck();
@@ -19,13 +30,13 @@ public class Chicken {
         eat();
     }
     private void cluck() {
-        System.out.println("This chicken is clucking");
+        System.out.println("This Chicken is Clucking");
     }
     private void wander(){
-        System.out.println("This chicken is wandering");
+        System.out.println("Thhis Chicken is wandering...");
     }
     private void eat(){
-        System.out.println("This chicken is eating");
+        System.out.println("This Chicken is eating...");
     }
     private void drink(){
         System.out.println("This chicken is drinking");
@@ -38,94 +49,64 @@ public class Chicken {
     
     private Egg layAnEgg(){
         
-        return new Egg();
+        return new Egg(1);
         
-    }
+    }   
     
     public Chicken() {
-        id =0;
+        id=0;
         name="ch";
-        color="White";
+        color="white";
         age=0;
         isMolting=false;
     }
 
-    public Chicken(int id, String name, String color, int age, boolean isMolting) {
+     public Chicken(int id, String name, String color, int age, boolean isMolting) {
         this.id = id;
         this.name = name;
         this.color = color;
         this.age = age;
         this.isMolting = isMolting;
     }
-
     
-    /**
-     * @return the id
-     */
+ 
     public int getId() {
         return id;
     }
 
-    /**
-     * @param id the id to set
-     */
     public void setId(int id) {
         this.id = id;
     }
 
-    /**
-     * @return the name
-     */
     public String getName() {
         return name;
     }
 
-    /**
-     * @param name the name to set
-     */
     public void setName(String name) {
         this.name = name;
     }
 
-    /**
-     * @return the color
-     */
     public String getColor() {
         return color;
     }
 
-    /**
-     * @param color the color to set
-     */
     public void setColor(String color) {
         this.color = color;
     }
 
-    /**
-     * @return the age
-     */
     public int getAge() {
         return age;
     }
 
-    /**
-     * @param age the age to set
-     */
     public void setAge(int age) {
         this.age = age;
     }
 
-    /**
-     * @return the asMolting
-     */
-    public boolean isAsMolting() {
+    public boolean isIsMolting() {
         return isMolting;
     }
 
-    /**
-     * @param isMolting the asMolting to set
-     */
-    public void setAsMolting(boolean isMolting) {
+    public void setIsMolting(boolean isMolting) {
         this.isMolting = isMolting;
     }
 }
