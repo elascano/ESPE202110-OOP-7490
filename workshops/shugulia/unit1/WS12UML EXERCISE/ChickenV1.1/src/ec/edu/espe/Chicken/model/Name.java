ccf02648b8c6b7645cfdf6732eea5b1f98851659
jgsimbana <gusjs84@gmail.com>
2021-12-11 18:12:34 -0500

    shugulia

---
+++ workshops/shugulia/unit1/WS12UML EXERCISE/ChickenV1.1/src/ec/edu/espe/Chicken/model/Name.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.Chicken.model;
+
+import java.io.InputStream;
+
+/**
+ *
+ * @author DARK
+ */
+public class Name {
+
+    public Name(InputStream in) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
