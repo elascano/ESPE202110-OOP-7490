4d7cb28fc478c580f4b347a0eda6b51da265bcae
PaulSaltos <paul.saltos000@gmail.com>
2021-12-11 18:48:55 -0500

    Carpetas

--- assignments/saltosp/unit1/HW07 Project/GroupProject/src/ec/edu/espe/controlWeapon/model/Code.java
+++ assignments/saltosp/unit1/HW07 Project/GroupProject/src/ec/edu/espe/controlWeapon/model/Code.java
@@ -12,6 +12,34 @@ public class Code {
     private String date;
     private String id;
 
+    @Override
+    public String toString() {
+        return "Code{" + "name=" + getName() + ", model=" + getModel() + ", type=" + getType() + ", date=" + getDate() + ", id=" + getId() + '}';
+    }
+
+    public Code(String name, String model, String type, String date, String id) {
+        this.name = name;
+        this.model = model;
+        this.type = type;
+        this.date = date;
+        this.id = id;
+    }
+    
+    public Code() {
+        
+        System.out.println("\n---------------> DATA Ammunition1 <--------------");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("---  name  --> ");
+        name = scanner.next();
+        System.out.println("---  color --> ");
+        color = scanner.next();
+        System.out.println("---  model --> ");
+        model = scanner.next();
+ 
+        
+    
+}
+
     public void doStuff() {
 
     }
@@ -22,5 +50,75 @@ public class Code {
 
     private void reception() {
 
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the model
+     */
+    public String getModel() {
+        return model;
+    }
+
+    /**
+     * @param model the model to set
+     */
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the date
+     */
+    public String getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
     }
 }
---
+++ workshops/saltosp/unit1/WS12UmlExercise/UmlExercise/src/ec/edu/espe/uml/model/E.java
@@ -0,0 +1,14 @@
+package ec.edu.espe.uml.model;
+
+/**
+ *
+ * @author Paul Saltos
+ */
+public class E {
+
+    @Override
+    public String toString() {
+        return "E{" + '}';
+    }
+    
+}
