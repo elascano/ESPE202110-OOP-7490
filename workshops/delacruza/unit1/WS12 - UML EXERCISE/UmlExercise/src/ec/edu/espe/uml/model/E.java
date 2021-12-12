0ea60f418b11384a954bf7389d130bf4271cac36
Aleshuga1020 <delacruz.sadlcq@gmail.com>
2021-12-11 19:10:37 -0500

    unittt

---
+++ workshops/delacruza/unit1/WS12 - UML EXERCISE/UmlExercise/src/ec/edu/espe/uml/model/E.java
@@ -0,0 +1,15 @@
+
+package ec.edu.espe.uml.model;
+
+/**
+ *
+ * @author Alejandro De La Cruz
+ */
+public class E {
+
+    @Override
+    public String toString() {
+        return "E{" + '}';
+    }
+    
+}
