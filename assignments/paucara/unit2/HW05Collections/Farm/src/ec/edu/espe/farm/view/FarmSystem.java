961800f462bd09db6bf55e7c87fe13454c2c2bd4
ajpaucar1 <javierpaucar16@gmail.com>
2021-12-13 17:53:01 -0500

    WS05V2

--- assignments/paucara/unit2/HW05Collections/Farm/src/ec/edu/espe/farm/view/FarmSystem.java
+++ assignments/paucara/unit2/HW05Collections/Farm/src/ec/edu/espe/farm/view/FarmSystem.java
@@ -1,10 +1,8 @@
 package ec.edu.espe.Farm.view;
 
-import ec.edu.espe.menu.model.Chicken;
-import ec.edu.espe.menu.model.ChickenCoop;
-
+import ec.edu.espe.Farm.model.Chicken;
+import ec.edu.espe.Farm.model.ChickenCoop;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Scanner;
 
 /**
@@ -14,46 +12,41 @@ import java.util.Scanner;
 public class FarmSystem {
 
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
         int number;
         int option;
         int id = 0;
-        String name = "";
-        String color = "";
         int age = 0;
         boolean molting = true;
+        String name = "";
+        String color = "";
+        Scanner teclado = new Scanner(System.in);
 
         Chicken chicken;
         ChickenCoop chickenCoop;
         ArrayList<Chicken> chickens = new ArrayList<>();
         chickenCoop = new ChickenCoop(id, chickens);
-        
-        System.out.println("--> HW05: Camila Teca <-- ");
-        System.out.println("--> Menu for the Farm System <--");
-        System.out.println("What do you want to do? -> Open Menu: Yes(1) / No (0)");
+
+        System.out.println(" WS05 Collections -----> Javier Paucar <----- ");
+        System.out.println("-----> DATA CHICKEN <-----");
+        System.out.println("Do you want to enter the menu? ->  \n 1 --> YES \n 0 --> NO");
         number = teclado.nextInt();
         while (number == 1) {
-            System.out.println("1.Add Chickens\n2.Remove Chickens\n3.Show Chickens in the coop\n4.Show the amount of chickens\n5.Exit\n");
+            System.out.println("Options:");
+            System.out.println("\n1--> Add Chickens\n2--> Remove Chickens for ID \n3--> Show Chickens in the coop\n4--> Show the amount of chickens\n5--> Exit\n");
             option = teclado.nextInt();
 
             if (option == 1) {
-                System.out.println("--> Enter data of the chicken <-- ");
-
+                System.out.println("---> Please enter the chicken data <--- ");
                 System.out.println("Id --> ");
                 id = teclado.nextInt();
-
                 System.out.println("Name --> ");
                 name = teclado.next();
-
                 System.out.println("Color --> ");
                 color = teclado.next();
-
                 System.out.println("Age --> ");
                 age = teclado.nextInt();
-
                 System.out.println("Molting --> ");
                 molting = teclado.nextBoolean();
-
                 chicken = new Chicken(id, name, color, age, molting);
                 chickenCoop.add(chicken);
                 System.out.println("\n chicken -->" + chicken);
@@ -67,13 +60,13 @@ public class FarmSystem {
                 System.out.println("\n");
             }
             if (option == 3) {
-                System.out.println("--> Chickens in the coop <-- ");
+                System.out.println("---> Chickens in the coop <--- ");
                 chicken = new Chicken(id, name, color, age, molting);
                 System.out.println("chicken coop -> " + chickenCoop);
                 System.out.println("\n");
             }
             if (option == 4) {
-                System.out.println("--> Amount of chickens <-- ");
+                System.out.println("---> Amount of chickens <--- ");
                 chicken = new Chicken(id, name, color, age, molting);
                 System.out.println("Chickens in the Chicken coop -> " + chickenCoop.countChickens());
                 System.out.println("\n");
