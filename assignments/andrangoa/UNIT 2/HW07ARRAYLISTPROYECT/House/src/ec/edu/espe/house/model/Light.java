666a9fcc322f9b4d0e4cc515d46d758a0f7c24c8
Adguaman <tuangelitop2020@gmail.com>
2021-12-13 21:21:35 -0500

    Unit2

---
+++ assignments/andrangoa/UNIT 2/HW07ARRAYLISTPROYECT/House/src/ec/edu/espe/house/model/Light.java
@@ -0,0 +1,54 @@
+
+package ec.edu.espe.house.model;
+
+/**
+ *
+ * @author alex_
+ */
+public class Light {
+    private int lightId;
+    private boolean statusLight;
+
+    @Override
+    public String toString() {
+        return "Light{" + "lightId=" + lightId + ", statusLight=" + statusLight + '}';
+    }
+
+    public Light(int lightId, boolean statusLight) {
+        this.lightId = lightId;
+        this.statusLight = statusLight;
+    }
+
+    
+    /**
+     * @return the lightId
+     */
+    public int getLightId() {
+        return lightId;
+    }
+
+    /**
+     * @param lightId the lightId to set
+     */
+    public void setLightId(int lightId) {
+        this.lightId = lightId;
+    }
+
+    /**
+     * @return the statusLight
+     */
+    public boolean isStatusLight() {
+        return statusLight;
+    }
+
+    /**
+     * @param statusLight the statusLight to set
+     */
+    public void setStatusLight(boolean statusLight) {
+        this.statusLight = statusLight;
+    }
+
+   
+    
+    
+}
