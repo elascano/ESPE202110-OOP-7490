ae23599af66729b540191bbab5542be45a860272
Ajshuguli <alanshuguli@outlook.com>
2022-01-05 20:39:13 -0500

    shugulia

---
+++ workshops/shugulia/unit2/WS18Json/Farm/src/ec/edu/espe/farm/model/FarmAnimal.java
@@ -0,0 +1,69 @@
+
+package ec.edu.espe.farm.model;
+
+import java.time.LocalDate;
+
+
+
+/**
+ *
+ * @author alan
+ */
+public class FarmAnimal {
+    private int id;
+    private String breed;
+    private LocalDate bornOn;
+    
+    
+    
+     public FarmAnimal(int id, String breed, LocalDate bornOn) {
+        this.id = id;
+        this.breed = breed;
+        this.bornOn = bornOn;
+    }
+
+    
+    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the breed
+     */
+    public String getBreed() {
+        return breed;
+    }
+
+    /**
+     * @param breed the breed to set
+     */
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    /**
+     * @return the bornOn
+     */
+    public LocalDate getBornOn() {
+        return bornOn;
+    }
+
+    /**
+     * @param bornOn the bornOn to set
+     */
+    public void setBornOn(LocalDate bornOn) {
+        this.bornOn = bornOn;
+    }
+    
+}
