46ea6db97a4d6a935b8dfdb3b0f38897d2423d8f
CristhianA2 <caaltamirano2@espe.edu.ec>
2022-01-11 08:22:12 -0500

    ExamnUnit2

---
+++ workshops/cadenab/Unit1/WS12UMLExercise/UmlExcersice/src/ec/edu/espe/uml/view/UmlExcersice.java
@@ -0,0 +1,40 @@
+
+package ec.edu.espe.uml.view;
+
+import ec.edu.espe.uml.model.A;
+import ec.edu.espe.uml.model.B;
+import ec.edu.espe.uml.model.C;
+import ec.edu.espe.uml.model.D;
+import ec.edu.espe.uml.model.E;
+
+/**
+ *
+ * @author Benjamin Cadena OOPTech ESPE-DCCO
+ */
+public class UmlExcersice {
+    
+    public static void main(String[] args) {
+        A a;
+        B b;
+        C c;
+        D d;
+        E e;
+        int p =10;
+        String q = "Hello Benjamin";
+        C cs[] = new C[15];
+        D[] ds = new D[10];
+       
+        a = new A(p, q);
+        b = new B(cs);
+        c = new C(ds);
+        d = new D();
+        e = new E();
+        
+        System.out.println("a -->" + a); 
+        a.m(b);
+        
+        System.out.println("c-->" +c);
+        System.out.println("d-->" +d);
+        System.out.println("e-->" +e);
+    }
+}
