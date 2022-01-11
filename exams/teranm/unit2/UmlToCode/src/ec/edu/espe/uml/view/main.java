05496c18d5bb23626ddb160435266295119be9e1
melanieteran <melanieteran19@gmail.com>
2022-01-11 08:57:08 -0500

    UmlToCoce

---
+++ exams/teranm/unit2/UmlToCode/src/ec/edu/espe/uml/view/main.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.uml.view;
+
+/**
+ *
+ * @author Melanie
+ */
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Exam");
+    }
+
+}
