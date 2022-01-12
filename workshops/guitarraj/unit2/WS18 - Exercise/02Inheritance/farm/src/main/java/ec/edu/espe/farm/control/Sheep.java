c22366d33767f6aaaa6faf4d90e99dbd2f49f404
lionarts16 <leoalexander16agos@gmail.com>
2022-01-12 00:31:12 -0500

    HW

---
+++ workshops/guitarraj/unit2/WS18 - Exercise/02Inheritance/farm/src/main/java/ec/edu/espe/farm/control/Sheep.java
@@ -0,0 +1,32 @@
+package ec.edu.espe.farm.control;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Guitarra Jhon
+ */
+public class Sheep extends FarmAnimal{
+    private LocalDate lastSheering;
+
+    public Sheep(int id, String breed, LocalDate bornOn,LocalDate lastSheering) {
+        super(id, breed, bornOn);
+        this.lastSheering = lastSheering;
+    }
+
+    /**
+     * @return the lastSheering
+     */
+    public LocalDate getLastSheering() {
+        return lastSheering;
+    }
+
+    /**
+     * @param lastSheering the lastSheering to set
+     */
+    public void setLastSheering(LocalDate lastSheering) {
+        this.lastSheering = lastSheering;
+    }
+    
+    
+}
