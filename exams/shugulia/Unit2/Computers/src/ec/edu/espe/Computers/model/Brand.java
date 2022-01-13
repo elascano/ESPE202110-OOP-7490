13919d16525d2c5bdc0da89332a9968abc413acf
Ajshuguli <alanshuguli@outlook.com>
2022-01-13 11:06:26 -0500

    shugulia

---
+++ exams/shugulia/Unit2/Computers/src/ec/edu/espe/Computers/model/Brand.java
@@ -0,0 +1,29 @@
+
+package ec.edu.espe.Computers.model;
+
+/**
+ *
+ * @author Alan
+ */
+public class Brand {
+    private String Stamp;
+
+    public Brand(String Stamp) {
+        this.Stamp = Stamp;
+    }
+
+    /**
+     * @return the Stamp
+     */
+    public String getStamp() {
+        return Stamp;
+    }
+
+    /**
+     * @param Stamp the Stamp to set
+     */
+    public void setStamp(String Stamp) {
+        this.Stamp = Stamp;
+    }
+    
+}
