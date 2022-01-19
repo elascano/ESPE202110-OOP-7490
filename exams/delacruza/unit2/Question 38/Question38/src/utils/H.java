8babdb12917579797ac6e86f0b972dd999010895
Aleshuga1020 <delacruz.sadlcq@gmail.com>
2022-01-19 08:46:33 -0500

    prueb2

---
+++ exams/delacruza/unit2/Question 38/Question38/src/utils/H.java
@@ -0,0 +1,13 @@
+
+package utils;
+
+import ec.edu.espe.quetion38.model.B;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ALEJANDRO DELACRUZ
+ */
+public interface H {
+    public ArrayList<B> bs = new ArrayList<>();    
+}
