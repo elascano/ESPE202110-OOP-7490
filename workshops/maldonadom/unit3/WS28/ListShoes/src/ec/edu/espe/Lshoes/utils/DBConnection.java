9133e5d0592bcff9dba223fa906f71703059b620
MateoMaldonado3 <maldonadom377@gmail.com>
2022-01-25 14:54:31 -0500

    .

---
+++ workshops/maldonadom/unit3/WS28/ListShoes/src/ec/edu/espe/Lshoes/utils/DBConnection.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.Lshoes.utils;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+/**
+ *
+ * @author andresmaldonado
+ */
+public class DBConnection {
+    
+    ConnectionString connectionString = new ConnectionString("mongodb+srv://msmaldonado3:sancayo12@clusteroop.x3i1d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+        MongoClientSettings settings = MongoClientSettings.builder()
+         .applyConnectionString(connectionString)
+          .build();
+        MongoClient mongoClient = MongoClients.create(settings);
+        MongoDatabase database = mongoClient.getDatabase("Workshop28");
+
+    
+    
+    public MongoDatabase obtenerDB(){
+        return database;
+        
+        
+    }
+    
+}
