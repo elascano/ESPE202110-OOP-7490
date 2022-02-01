ab152589361780998e8aa95f0082e63121778294
Adguaman <tuangelitop2020@gmail.com>
2022-01-31 22:31:41 -0500

    HW17Register

--- workshops/guamana/unit3/WS30/SmartCity/src/utils/ConnectionMarketTable.java
+++ workshops/guamana/unit3/WS30/SmartCity/src/utils/ConnectionMarketTable.java
@@ -1,5 +1,5 @@
 
-package ec.edu.SmartCity.Utils;
+package utils;
 
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
@@ -11,23 +11,16 @@ import com.mongodb.client.MongoDatabase;
  *
  * @author Angel Guaman
  */
-public class Connection {
+public class ConnectionMarketTable {
     
-      
+    
 ConnectionString connectionString = new ConnectionString("mongodb+srv://ANGELJR:3084@cluster0.sxlkt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 MongoClientSettings settings = MongoClientSettings.builder()
         .applyConnectionString(connectionString)
         .build();
 MongoClient mongoClient = MongoClients.create(settings);
-MongoDatabase database = mongoClient.getDatabase("FindTable");
+MongoDatabase database = mongoClient.getDatabase("MarketTable");
 
-
-
-
-
-
-    
-    
     public MongoDatabase obtenerDB(){
         return database;
     }
