a74bc26c61e25e74e4dc0e9d122a1fff4401cc08
psbustillos1 <bustilloss067@gmail.com>
2022-02-01 23:35:17 -0500

    PS

---
+++ assignments/bustillosp/unit3/HW17EventsValidateFields/Registerfinal/src/utils/Connection.java
@@ -0,0 +1,23 @@
+
+package utils;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+
+public class Connection {
+ConnectionString connectionString = new ConnectionString("mongodb+srv://PSBM:1751375971@cluster0.9shff.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+MongoClientSettings settings = MongoClientSettings.builder()
+        .applyConnectionString(connectionString)
+        .build();
+MongoClient mongoClient = MongoClients.create(settings);
+MongoDatabase database = mongoClient.getDatabase("RegisterFinal");
+
+    public MongoDatabase obtenerDB(){
+        return database;
+    }
+  
+}
---
+++ workshops/bustillosp/unit3/WS28ClassIndividualExcersice/Apartment/src/ec/espe/edu/InventorySystem/utils/Connection.java
@@ -0,0 +1,36 @@
+
+package ec.espe.edu.InventorySystem.utils;
+
+/**
+ *
+ * @author Bustillos
+ */
+
+
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+public class Connection {
+    
+ConnectionString connectionString = new ConnectionString("mongodb+srv://PSBM:1751375971@cluster0.9shff.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+MongoClientSettings settings = MongoClientSettings.builder()
+        .applyConnectionString(connectionString)
+        .build();
+MongoClient mongoClient = MongoClients.create(settings);
+MongoDatabase database = mongoClient.getDatabase("SalesSystem");
+  
+
+    
+    
+    public MongoDatabase obtenerDB(){
+        return database;
+    }
+    
+}
+
+
+
