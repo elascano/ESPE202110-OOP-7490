c65e71b20cd51d290d0a5c693fbdf89c70ec2a0a
PaulSaltos <paul.saltos000@gmail.com>
2022-02-14 21:36:20 -0500

    HW20Individual

---
+++ workshops/saltosp/unit3/WS33MathOperations/MathOperation/test/ec/edu/espe/MathOperation/BasicOperationTest.java
@@ -0,0 +1,47 @@
+
+package ec.edu.espe.MathOperation;
+
+import ec.edu.espe.MathOperation.BasicOperation;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Paul Saltos , DEEE-ESPE
+ */
+public class BasicOperationTest {
+    
+    public BasicOperationTest() {
+    }
+
+    /**
+     * Test of add method, of class BasicOperation.
+     */
+    @Test
+    public void testAdd() {
+        System.out.println("add");
+        float addend1 = 1.4F;
+        float addend2 = 2.4F;
+        BasicOperation instance = new BasicOperation();
+        float expResult = 3.8F;
+        float result = instance.add(addend1, addend2);
+        assertEquals(expResult, result, 0.01);
+        
+    }
+
+    /**
+     * Test of subtruct method, of class BasicOperation.
+     */
+    @Test
+    public void testSubtruct() {
+        System.out.println("subtruct");
+        float minuend = 0.0F;
+        float substraend = 0.0F;
+        BasicOperation instance = new BasicOperation();
+        float expResult = 0.0F;
+        float result = instance.subtruct(minuend, substraend);
+        assertEquals(expResult, result, 0.01);
+        
+    }
+    
+}
