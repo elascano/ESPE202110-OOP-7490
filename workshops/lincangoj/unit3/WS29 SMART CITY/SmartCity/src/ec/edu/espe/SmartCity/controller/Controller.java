6fd58723f12a08391189a17c0db33207e2261e40
jdLincango <lincangod882@gmail.com>
2022-02-14 22:25:58 -0500

    HW

--- workshops/lincangoj/unit3/WS29 SMART CITY/SmartCity/src/ec/edu/espe/SmartCity/controller/Controller.java
+++ workshops/lincangoj/unit3/WS29 SMART CITY/SmartCity/src/ec/edu/espe/SmartCity/controller/Controller.java
@@ -4,10 +4,31 @@
  */
 package ec.edu.espe.SmartCity.controller;
 
-/**
- *
- * @author MATRIZ PC
- */
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import java.util.ArrayList;
+import javax.swing.table.DefaultTableModel;
+import org.bson.Document;
+
+
+
 public class Controller {
     
+    public static void find(String name , DefaultTableModel table) 
+    {
+        MongoCollection<Document> Bone = new Connection().obtenerDB().getCollection("Bones Information");
+        Document document = new Document();
+        document.put("NAME", name);
+
+        MongoCursor<Document> query = Bone.find(document).iterator();
+        
+        while(query.hasNext())
+        {
+
+            ArrayList<Object> doc = new ArrayList<Object>(query.next().values());
+           table.addRow(doc.toArray());
+        }
+        
+
+    }
 }
