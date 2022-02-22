c6edea51f7fefc61307fc0c800d11d067db0545d
lionarts16 <leoalexander16agos@gmail.com>
2022-02-22 18:26:57 -0500

    Leo

---
+++ assignments/quingal/unit3/HW20IndividualExerciseTestCases/CalculateWeight/src/ec/edu/espe/calculateWeight/CalculateWeight.java
@@ -0,0 +1,73 @@
+
+package ec.edu.espe.calculateWeight;
+
+/**
+ *
+ * @author Quinga Leandro DEEE-ESPE
+ */
+public class CalculateWeight {
+   public float weight1(float mass,float gravity){
+        float multiplication;
+        //a complex computig algorithm is here 
+        multiplication = mass * gravity;
+        return multiplication;
+    } 
+   
+    public float weight2(float mass,float gravity){
+        float multiplication;
+        //a complex computig algorithm is here 
+        multiplication = mass * gravity;
+        return multiplication;
+    } 
+    
+     public float weight3(float mass,float gravity){
+        float multiplication;
+        //a complex computig algorithm is here 
+        multiplication = mass * gravity;
+        return multiplication;
+    } 
+     
+      public float weight4(float mass,float gravity){
+        float multiplication;
+        //a complex computig algorithm is here 
+        multiplication = mass * gravity;
+        return multiplication;
+    } 
+       public float weight5(float mass,float gravity){
+        float multiplication;
+        //a complex computig algorithm is here 
+        multiplication = mass * gravity;
+        return multiplication;
+    } 
+        public float weight6(float mass,float gravity){
+        float multiplication;
+        //a complex computig algorithm is here 
+        multiplication = mass * gravity;
+        return multiplication;
+    } 
+         public float weight7(float mass,float gravity){
+        float multiplication;
+        //a complex computig algorithm is here 
+        multiplication = mass * gravity;
+        return multiplication;
+    } 
+          public float weight8(float mass,float gravity){
+        float multiplication;
+        //a complex computig algorithm is here 
+        multiplication = mass * gravity;
+        return multiplication;
+    } 
+           public float weight9(float mass,float gravity){
+        float multiplication;
+        //a complex computig algorithm is here 
+        multiplication = mass * gravity;
+        return multiplication;
+    } 
+        public float weight10(float mass,float gravity){
+        float multiplication;
+        //a complex computig algorithm is here 
+        multiplication = mass * gravity;
+        return multiplication;
+    } 
+   
+}
