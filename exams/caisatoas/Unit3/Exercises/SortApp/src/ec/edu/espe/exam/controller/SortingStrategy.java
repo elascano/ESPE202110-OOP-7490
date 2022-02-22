50628d4cd143c86b1a354790e6bfb43f88562dcb
Seebiitaas <89808381+Seebiitaas@users.noreply.github.com>
2022-02-22 18:58:12 -0500

    Exam

---
+++ exams/caisatoas/Unit3/Exercises/SortApp/src/ec/edu/espe/exam/controller/SortingStrategy.java
@@ -0,0 +1,10 @@
+
+package ec.edu.espe.exam.controller;
+
+/**
+ *
+ * @author Sebastian Caisatoa
+ */
+public interface SortingStrategy {
+    public int[] sort(int[] data);
+}
---
+++ workshops/caisatoas/Unit3/WS34SingletonPattern/CalculatorAndUSTax/src/ec/edu/espe/calculator/controller/calculator.java
@@ -0,0 +1,19 @@
+
+package ec.edu.espe.calculator.controller;
+
+import ec.edu.espe.calculator.model.CaliforniaTax;
+
+/**
+ *
+ * @author Sebastian Caisatoa
+ */
+public class calculator {
+    
+    public static void main(String[] args) {
+        /*
+        CaliforniaTax tax = CaliforniaTax.getInstance();
+        tax.salesTotal();  
+        */
+    }
+    
+}
