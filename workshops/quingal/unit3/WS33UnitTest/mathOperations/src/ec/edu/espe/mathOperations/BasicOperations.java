ea41118e92fe4ad66c9dd461b11981af71f15887
lionarts16 <leoalexander16agos@gmail.com>
2022-02-22 18:35:01 -0500

    EXAM

---
+++ workshops/quingal/unit3/WS33UnitTest/mathOperations/src/ec/edu/espe/mathOperations/BasicOperations.java
@@ -0,0 +1,23 @@
+
+package ec.edu.espe.mathOperations;
+
+/**
+ *
+ * @author Quinga Leandro
+ */
+public class BasicOperations {
+    public float add(float addend1,float addend2){
+        float sum;
+        //a complex computig algorithm is here 
+        sum = addend1 + addend2;
+        return sum;
+    }
+    public float subtruct(float minuend,float substraend){
+        float difference;
+        //a complex computing algorithm is here
+        difference = minuend - substraend;
+        return difference;        
+    }
+    
+    
+}
