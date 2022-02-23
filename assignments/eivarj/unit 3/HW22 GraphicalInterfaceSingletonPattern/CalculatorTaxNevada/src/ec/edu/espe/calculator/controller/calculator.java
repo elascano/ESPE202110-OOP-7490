2ce13b4cc956cefd362963c7574357868cb3d619
unknown <Jaimemeivard@hotmail.com>
2022-02-22 22:27:59 -0500

    Unit3

---
+++ assignments/eivarj/unit 3/HW22 GraphicalInterfaceSingletonPattern/CalculatorTaxNevada/src/ec/edu/espe/calculator/controller/calculator.java
@@ -0,0 +1,19 @@
+
+package ec.edu.espe.calculator.controller;
+
+import ec.edu.espe.calculator.model.KansasRate;
+
+/**
+ *
+ * @author Eivar Jaime
+ */
+public class calculator {
+    
+    public static void main(String[] args) {
+        /*
+        NevadaTax tax = KansasrategetInstance();
+        rate.salesTotal();  
+        */
+    }
+    
+}
