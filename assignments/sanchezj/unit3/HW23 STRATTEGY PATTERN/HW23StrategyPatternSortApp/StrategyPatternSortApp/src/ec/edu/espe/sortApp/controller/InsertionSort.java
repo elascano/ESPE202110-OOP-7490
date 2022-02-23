b41c5bb6a08b5d3cafd1163ed0e4e84b213ef6ef
SanchezJoseFrancisco93 <sanchezmishquero@gmail.com>
2022-02-23 06:24:57 -0500

    x

---
+++ assignments/sanchezj/unit3/HW23 STRATTEGY PATTERN/HW23StrategyPatternSortApp/StrategyPatternSortApp/src/ec/edu/espe/sortApp/controller/InsertionSort.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.sortApp.controller;
+
+/**
+ *
+ * @author Jose Sanchez, DEEL-ESPE
+ */
+public class InsertionSort implements SortingStrategy {
+    
+    public int[] sort(int[] data) {
+       
+         for (int i=1; i < data.length; i++) {
+         int aux = data[i];
+         int j;
+         for (j=i-1; j >=0 && data[j] > aux; j--){
+              data[j+1] = data[j];
+          }
+         data[j+1] = aux;
+      }
+        return data;
+       
+    }
+}
