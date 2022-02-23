c8ecf882995f2f4ce6bb0c395597f69c85f67caf
AxAndrango <alexandrango2303@gmail.com>
2022-02-22 22:21:14 -0500

    update repo

---
+++ exams/andrangoa/UNIT 3/FinalExamExcercise/SortingAlgorithms/src/ec/edu/espe/model/List.java
@@ -0,0 +1,30 @@
+
+package ec.edu.espe.model;
+
+/**
+ *
+ * @author Alex Andrango
+ */
+public class List {
+    private int number;
+
+    /**
+     * @return the number
+     */
+    public int getNumber() {
+        return number;
+    }
+
+    /**
+     * @param number the number to set
+     */
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public List(int number) {
+        this.number = number;
+    }
+    
+    
+}
---
+++ workshops/andrangoa/UNIT 3/WS33UnitTest/taxes/src/ec/edu/espe/mathOperations/BasicOperation.java
@@ -0,0 +1,14 @@
+
+package ec.edu.espe.mathOperations;
+
+/**
+ *
+ * @author Alex Andrango
+ */
+public class BasicOperation {
+    public float add(float addend1, float addend2){
+       float sum;
+       sum = addend1 + addend2;
+       return sum;
+    }            
+}
