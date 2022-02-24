3a7bd108de75798743f38793c1b2ad0f7fd34f5a
Aleshuga1020 <delacruz.sadlcq@gmail.com>
2022-02-24 18:19:58 -0500

    FINAL

---
+++ workshops/delacruza/unit3/WS33 Math Operations/MathOperations/src/ec/edu/espe/mathoperations/BasicOperations.java
@@ -0,0 +1,30 @@
+package ec.edu.espe.mathoperations;
+
+/**
+ *
+ * @author ALEJANDRO DELACRUZ
+ */
+public class BasicOperations {
+    
+    //Funcion para que pueda se probada, debe al menos
+    // Recibir un parametro
+    // Retornar algo
+    
+    public float add(float addend1, float addend2){
+        float sum;
+        
+        // a complex computing algoritm is here
+        sum = addend1 + addend2;
+        return sum;
+        
+        }
+        
+    public float subtruct(float minuend, float substraend){
+        float difference;
+        
+        // a complex computing algoritm is here
+        difference = minuend - substraend;
+        return difference;
+    }
+    
+}
