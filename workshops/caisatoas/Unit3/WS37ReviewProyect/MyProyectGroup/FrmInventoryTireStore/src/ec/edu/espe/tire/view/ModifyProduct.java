2725ad56b9c5cf2a1883872cf4253175801cb028
Seebiitaas <89808381+Seebiitaas@users.noreply.github.com>
2022-02-26 01:05:31 -0500

    Unit 3

---
+++ workshops/caisatoas/Unit3/WS37ReviewProyect/MyProyectGroup/FrmInventoryTireStore/src/ec/edu/espe/tire/view/ModifyProduct.java
@@ -0,0 +1,282 @@
+
+package ec.edu.espe.tire.view;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.result.DeleteResult;
+import utils.Connection;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+/**
+ *
+ * @author Caisatoa
+ */
+public class ModifyProduct extends javax.swing.JFrame {
+
+    MongoCollection<Document> PurchaseRecord = new Connection().obtenerDB().getCollection("PurchaseRecord");    
+    DefaultTableModel tabla = new DefaultTableModel(){
+        @Override
+        public boolean isCellEditable(int row, int column) {
+            return false; //To change body of generated methods, choose Tools | Templates.
+        }
+        
+    };
+    public ModifyProduct() {
+        initComponents();
+        tblContacts.setModel(tabla);
+        
+        tabla.addColumn("ID CLOUD");tabla.addColumn("ID TIRE");tabla.addColumn("NAME");tabla.addColumn("QUANTITY");
+        tabla.addColumn("PRICE UNITARY");tabla.addColumn("TOTAL PRICE");tabla.addColumn("CONDITION");
+        tabla.addColumn("DATE");tabla.addColumn("COMMENT");
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        tblContacts = new javax.swing.JTable();
+        btnDelete = new javax.swing.JButton();
+        btnModify = new javax.swing.JButton();
+        btnReturn = new javax.swing.JButton();
+        btnUpdate = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(255, 102, 102));
+
+        tblContacts.setFont(new java.awt.Font("Bodoni MT", 0, 14)); // NOI18N
+        tblContacts.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane2.setViewportView(tblContacts);
+
+        btnDelete.setBackground(new java.awt.Color(255, 0, 0));
+        btnDelete.setFont(new java.awt.Font("Bodoni MT", 3, 14)); // NOI18N
+        btnDelete.setText("Delete");
+        btnDelete.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDeleteActionPerformed(evt);
+            }
+        });
+
+        btnModify.setBackground(new java.awt.Color(255, 255, 102));
+        btnModify.setFont(new java.awt.Font("Bodoni MT", 3, 14)); // NOI18N
+        btnModify.setText("Modify");
+        btnModify.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnModifyActionPerformed(evt);
+            }
+        });
+
+        btnReturn.setBackground(new java.awt.Color(102, 102, 255));
+        btnReturn.setFont(new java.awt.Font("Bodoni MT", 3, 14)); // NOI18N
+        btnReturn.setText("Return");
+        btnReturn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnReturnActionPerformed(evt);
+            }
+        });
+
+        btnUpdate.setBackground(new java.awt.Color(102, 255, 102));
+        btnUpdate.setFont(new java.awt.Font("Bodoni MT", 3, 14)); // NOI18N
+        btnUpdate.setText("Upload Table");
+        btnUpdate.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnUpdateActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(24, 24, 24)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(btnUpdate)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnDelete, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(33, 33, 33)
+                        .addComponent(btnModify)
+                        .addGap(33, 33, 33)
+                        .addComponent(btnReturn))
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 843, Short.MAX_VALUE))
+                .addGap(26, 26, 26))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 162, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(btnReturn)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(btnDelete, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnUpdate)
+                        .addComponent(btnModify, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addContainerGap(16, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnDeleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDeleteActionPerformed
+        reset();
+    }//GEN-LAST:event_btnDeleteActionPerformed
+
+    private void btnReturnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnReturnActionPerformed
+        FrmInventoryStart frmContacts = new FrmInventoryStart();
+        frmContacts.setVisible(true);
+        frmContacts.setLocationRelativeTo(null);
+        this.dispose();
+    }//GEN-LAST:event_btnReturnActionPerformed
+
+    private void btnUpdateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnUpdateActionPerformed
+        update();
+    }//GEN-LAST:event_btnUpdateActionPerformed
+
+    private void btnModifyActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnModifyActionPerformed
+        modify();
+    }//GEN-LAST:event_btnModifyActionPerformed
+
+    public void view(){
+        
+        MongoCursor<Document> consult = PurchaseRecord.find().iterator();
+        
+        int total = tabla.getRowCount();
+        for(int i = 0; i<total; i++){
+            tabla.removeRow(0);
+        }
+        while(consult.hasNext()){
+            ArrayList<Object> doc = new ArrayList<Object>(consult.next().values());
+            tabla.addRow(doc.toArray());
+        }
+    }
+    
+    public boolean Delete(String id){
+        DeleteResult answer = PurchaseRecord.deleteOne(new Document("_id", new ObjectId(id)));
+        if(answer.getDeletedCount()==1){
+            return true;
+        }
+        return false;
+    }    
+    
+    public void reset(){
+        int renglon = tblContacts.getSelectedRow();
+        if(renglon == -1){
+            JOptionPane.showMessageDialog(this, "Error, Selects the Line to Delete");
+            return;
+        }
+        String idRemove = tblContacts.getValueAt(renglon, 0).toString(); 
+        int respuesta = JOptionPane.showConfirmDialog(this, "Are you sure to delete the ID?"+ idRemove);
+        if(respuesta == JOptionPane.OK_OPTION){
+            boolean answerDelete = Delete(idRemove);
+            if(answerDelete==true){
+                view();
+                JOptionPane.showMessageDialog(this, "Was Removed Successfully");
+            }else{
+                JOptionPane.showMessageDialog(this, "Could Not Be Deleted");
+
+            }
+        }
+    }
+    public void modify(){
+        int renglon = tblContacts.getSelectedRow();
+        if(renglon == -1){
+            JOptionPane.showMessageDialog(this, "Error, Select the row to update");
+            return;
+        }
+        String idUpload = tblContacts.getValueAt(renglon, 0).toString();
+        int answer = JOptionPane.showConfirmDialog(this, "Are you sure to update the ID?"+ idUpload);
+        if(answer == JOptionPane.OK_OPTION){
+            Upload upload = new Upload(this,true, PurchaseRecord, idUpload);
+            upload.setVisible(true);
+            view();
+        }
+    }
+    public void update(){
+        MongoCursor<Document> consult = PurchaseRecord.find().iterator();
+        
+        int total = tabla.getRowCount();
+        for(int i = 0; i<total; i++){
+            tabla.removeRow(0);
+        }
+        while(consult.hasNext()){
+            ArrayList<Object> doc = new ArrayList<Object>(consult.next().values());
+            tabla.addRow(doc.toArray());
+        }
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(ModifyProduct.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(ModifyProduct.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(ModifyProduct.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ModifyProduct.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ModifyProduct().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnDelete;
+    private javax.swing.JButton btnModify;
+    private javax.swing.JButton btnReturn;
+    private javax.swing.JButton btnUpdate;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTable tblContacts;
+    // End of variables declaration//GEN-END:variables
+}
